3abf866c0bb1e9238f938fb1b3acfab34489c07e
boys <474573550@qq.com>
2020-12-09 18:01:10 +0800

    wancheng ajksjdjh

--- pages/detailed.js
+++ pages/detailed.js
@@ -22,45 +22,8 @@ import "../static/pages/detailed.css"
 import Axios from "axios"
 
 
-let markdown='# P01:课程介绍和环境搭建\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-   '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n'+
-  '\`console.log(111)\` \n\n'+
-  '# p02:来个Hello World 初始Vue3.0\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n'+
-  '***\n\n\n' +
-  '# p03:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p04:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '#5 p05:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p06:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p07:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '``` var a=11; ```'
-
-
-
 const Detailed = (props) => {
-  console.log(props)
+
   const renderer = new marked.Renderer()
   marked.setOptions({
     renderer:renderer,
@@ -75,7 +38,7 @@ const Detailed = (props) => {
       return hljs.highlightAuto(code).value
     }
   })
-let html = marked(markdown)
+let html = marked(props.article_content)
 
 
   return (
@@ -152,8 +115,7 @@ Detailed.getInitialProps = async (context)=> {
 let id=context.query.id
 const promise = new Promise((resolve)=>{
   Axios("http://localhost:7001/default/getArticleById/"+id).then(res=>{
-    console.log(res)
-    resolve(res.data)
+    resolve(res.data.data[0])
   })
 })
 return await promise
